49cb698aabf483855884c582f3e4ad30150242ab
Nathan Probert <nathanprobert@rogers.com>
2024-10-25 00:05:56 -0400

    ugh

--- next.config.mjs
+++ next.config.mjs
@@ -2,5 +2,6 @@
 export default {
     pageExtensions: ['ts', 'tsx', 'js', 'jsx'],  // Include all file extensions you're using
     reactStrictMode: true,  // Optional, recommended for development
+    output: 'standalone',
   };
   
