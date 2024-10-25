d798d88d45006556ced7fbea2d9f8400ab02dd48
Nathan Probert <nathanprobert@rogers.com>
2024-10-24 23:57:09 -0400

    please

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.mjs
+export default {
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx'],  // Include all file extensions you're using
+    reactStrictMode: true,  // Optional, recommended for development
+  };
+  
