e033ba168b2cda2758bead878627562b009e97f5
Nathan Probert <nathanprobert@rogers.com>
2024-11-01 00:08:45 -0400

    add experience endpoint to maintain coop links

---
+++ app/experience/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Home() {
+
+
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/S25-Evertz");
+  }, [router]);
+
+  return null; // Optionally return null, or a loading state while redirecting
+}
