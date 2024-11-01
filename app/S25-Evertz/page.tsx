4e54372140153b6ecf3f68bdecf0cd6db9d355d5
Nathan Probert <109328434+nathan-probert@users.noreply.github.com>
2024-11-01 00:52:32 -0400

    Revert "Add experience endpoint to maintain coop links (#3)"
    This reverts commit 17b37e43c4e7f8260556a6bcc665cfe11aa62dfe.

--- app/S25-Evertz/page.tsx
+++ app/S25-Evertz/page.tsx
@@ -7,7 +7,7 @@ export const metadata = {
 export default function S25() {
     return (
         <div className="max-w-4xl mx-auto my-3 p-8 relative">
-          <img
+          <Image
             src="/images/evertz.png"
             alt="Evertz Logo"
             width={100}
@@ -42,7 +42,7 @@ export default function S25() {
               and more.
             </p>
             <br />
-            <img
+            <Image
               src="/images/coop-image.jpeg"
               alt="Co-op experience"
               width={800}
@@ -94,7 +94,7 @@ export default function S25() {
               &emsp;I would like to express my gratitude to the team at Evertz Microsystems for providing me with the opportunity...
             </p>
           </section>
-          <img
+          <Image
             src="/images/evertz-building.jpg"
             alt="Evertz Building"
             width={800}
