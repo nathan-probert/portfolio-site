86f516e8b36f17bc9305bae1834bd972cd0a7f51
Nathan Probert <nathanprobert@rogers.com>
2025-01-09 00:20:23 -0500

    Not all files were added

---
+++ app/F24-Evertz/page.tsx
@@ -0,0 +1,157 @@
+
+export const metadata = {
+    title: 'F24 - Evertz',
+}
+
+export default function F24() {
+    return (
+        <div className="max-w-4xl mx-auto my-3 p-8 relative">
+          <img
+            src="/images/evertz.png"
+            alt="Evertz Logo"
+            width={100}
+            height={100}
+            className="absolute top-0 right-0 mr-6"
+          />
+          <header className="mb-6">
+            <h1 className="text-4xl font-bold">Evertz - Fall 2024</h1>
+            <p className="text-sm">Published on: January 7, 2025</p>
+          </header>
+          <section className="mb-6">
+            <h2 className="text-2xl font-semibold mb-4 text-grey2 pb-1 border-b border-grey2">Introduction</h2>
+            <p className="">
+              &emsp;From May to December 2024, I served as a Software Developer Intern at Evertz Microsystems Ltd. in Burlington,
+               Ontario. This report offers an overview of the organization, highlights my personal learning goals, and details 
+               my responsibilities and accomplishments during the second half of my work term, September to December.
+            </p>
+          </section>
+          <section className="mb-6">
+            <h2 className="text-2xl font-semibold mb-4 text-grey2 pb-1 border-b border-grey2">Information About Evertz</h2>
+            <p className="">
+              &emsp;Evertz Microsystems Ltd. is a leading global manufacturer of broadcast and
+              media production equipment, offering comprehensive end-to-end solutions for broadcast,
+              satellite, cable TV, and telecommunications industries.
+              <br />
+              <br />
+              &emsp;Evertz.io is a Software as a Service platform that allows cloud video services for
+              content owners and creators. This service fully manages platform maintenance, backend
+              engineering, and cloud scaling, allowing customers to focus their resources entirely on
+              operations. They work with companies such as Disney, Warner Bros, NBC, HBO, Paramount,
+              and more.
+            </p>
+            <br />
+            <img
+              src="/images/christmas_party_sign.jpg"
+              alt="Co-op experiencepicture"
+              width={800}
+              height={500}
+              className="w-full h-auto"
+            />
+          </section>
+          <section className="mb-6">
+            <h2 className="text-2xl font-semibold mb-4 text-grey2 pb-1 border-b border-grey2">Job Description</h2>
+            <p className="">
+              &emsp;In my role at Evertz, I worked mainly as a backend cloud developer, focusing on tasks involving AWS services,
+               parallel processing, and websocket communication. Many of the skills I used throughout my work term were developed 
+              on the job, rather than at school. However, I was able to apply the idea behind many of the concepts I learned in my
+              courses to my work at Evertz. I was provided with the opportunity to work on many unqiue projects, allowing me to learn
+              new skills and technologies.
+            </p>
+          </section>
+          <section className="mb-6">
+            <h2 className="text-2xl font-semibold mb-4 text-grey2 pb-1 border-b border-grey2">Goals</h2>
+            <div className="">
+              <h3 className="font-semibold">&emsp;-&emsp;Goal 1</h3>
+              <p className="ml-6 mb-4">
+                &emsp;Improve communication skills by participating in team meetings and brainstorming sessions. Seek feedback on 
+                communication styles from peers and supervisors. Speak up in stand ups and other meetings.
+                <br />
+                <br />
+                &emsp;	
+                To enhance my collaboration skills and effectively communicate within cross functional teams, I made a noticeable 
+                effort to contribute more during the planning phase of new projects. At the start of the term, I had no trouble 
+                discussing my own work and asking questions surrounding that, but I found myself failing to follow and effectively 
+                contribute to higher level discussions about the feature as a whole. Thankfully, I found myself with excellent 
+                opportunities to work on this as a new, large project was being introduced in early October. This project had 
+                many modular components, so I had to work together with others to create the final result. I used this chance 
+                to stay active in the discussion phase, asking clarifying questions and contributing where possible. Additionally, 
+                near the end of my term two other members of my team began to attend in-office, allowing me to hold discussions 
+                with them about different ideas and approaches for the projects. In summary, I was able to get lots of practice 
+                with my collaboration skills and discussing high-level approaches in meetings. I received positive feedback from 
+                coworkers, and the deeper understanding allowed me to better work on my individual PR&apos;s as I knew how it should 
+                interact with other parts of the project.
+                <br />
+                <br /> 
+              </p>
+              <h3 className="font-semibold">&emsp;-&emsp;Goal 2</h3>
+              <p className="ml-6 mb-4">
+                &emsp;	
+                Attend industry events, webinars, or workshops organized by Evertz. Schedule calls with colleagues to learn about 
+                their roles and experiences. Connect with coworkers on professional platforms (e.g. LinkedIn).
+                <br />
+                <br />
+                &emsp;	
+                One of my goals was to establish a professional network within Evertz, and to maintain this rapport throughout my 
+                term at Evertz and after as well. To accomplish this, I connected with many of my coworkers on Linkedin so that 
+                I could stay in touch with them. Additionally, I had coffee chats with some members of my team that I was closer 
+                with, and asked about their experience with both Evertz and in general. These proved to be very informative, and
+                 helped me connect better with my coworkers. I also used a group chat of co-op students to connect with them as well. 
+                 One measure of success for me was to attend a co-op event, and while it was difficult for me as events were in 
+                 Burlington (I work hybrid out of Toronto), I attended the Christmas party at the end of my term. This was an 
+                 excellent opportunity for me to build in-person connections with my team, co-ops, and other people that I work 
+                 with who aren&apos;t directly on my team. I managed to connect with a few more people on Linkedin from that experience. 
+                <br />
+                <br /> 
+              </p>
+              <h3 className="font-semibold">&emsp;-&emsp;Goal 3</h3>
+              <p className="ml-6 mb-4">
+                &emsp;Review PRs submitted by colleagues, focusing on coding standards, best practices, and project requirements.
+                <br />
+                <br />
+                &emsp;Another one of my goals was to enhance my leadership abilities, something that will be invaluable if I am 
+                hoping to have my own team one day. To do this I wanted to actively review my coworkers&apos; PR&apos;s, particularly with 
+                the goal of pointing out better approaches or solutions (rather than low impact fixes like removing print statements 
+                etc.). To achieve this, I set a schedule for myself where I did code review for a specific amount of time each day. 
+                This dedicated amount of time allowed me to write meaningful reviews and spark conversations around the way solutions 
+                were done. I was also able to spot areas where code could be optimized saving time and money. I received positive 
+                feedback for my efforts.
+                <br />
+                <br /> 
+              </p>
+            </div>
+          </section>
+          <section className="mb-6">
+            <h2 className="text-2xl font-semibold mb-4 text-grey2 pb-1 border-b border-grey2">Conclusion</h2>
+            <p className="">
+              &emsp;My co-op term at Evertz was a great learning experience. I got to dive into backend cloud development, 
+              work with AWS, and tackle some pretty cool projects. Along the way, I improved my communication and collaboration
+               skills, especially during team discussions and code reviews. I also built strong connections with my coworkers, 
+               which made the experience even better. Overall, this term gave me a solid foundation in cloud computing and 
+               reinforced my excitement for working in tech.
+            </p>
+          </section>
+          <section className="mb-6">
+            <h2 className="text-2xl font-semibold mb-4 text-grey2 pb-1 border-b border-grey2">Acknowledgements</h2>
+            <p className="">
+              &emsp;Prabhjot Singh, for his guidance and friendship throughout my work term. He was always available to answer 
+              any questions I had and provided valuable feedback on my work. I am grateful for his mentorship and support.
+              <br />
+              <br />
+              &emsp;Joydip Panda, for his patience and encouragement. He challenged me to think critically about my work and
+              improve as a developer. I appreciate his dedication to helping me grow in my role. 
+            </p>
+          </section>
+          <img
+            src="/images/party-photobooth.png"
+            alt="Christmas Party Photobooth Picture"
+            width={800}
+            height={500}
+            className="w-full h-auto"
+          />
+          <footer className="mt-6 text-right text-sm text-grey2 pb-1">
+            <p>Written by: Nathan Probert</p>
+          </footer>
+        </div>
+    );
+     
+}
