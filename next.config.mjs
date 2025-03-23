3d64bfcc25c1885b3a9ecfcfbedbb42764981b6d
Nathan Probert <109328434+nathan-probert@users.noreply.github.com>
2025-03-22 20:24:50 -0400

    This works in dev, need to test in prod (#17)
    * This works in dev, need to test in prod
    
    * try this

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
-// next.config.mjs
 export default {
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx'],  // Include all file extensions you're using
-    reactStrictMode: true,  // Optional, recommended for development
-    output: 'standalone',
-  };
-  
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  reactStrictMode: true,
+  output: 'standalone',
+  basePath: '',  // No base path for local or production. Keep it at the root level.
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',  // Don't change the asset prefix; keep it the same for development and production
+  async rewrites() {
+    return [
+      {
+        source: '/images/:path*',
+        destination: '/public/images/:path*',  // Images are served from the public folder
+      },
+    ];
+  },
+};
